11406b32f1898f337f460802f24f4f7a5ce8fc4b
Joao Augusto <augusto_casotti@hotmail.com>
2020-07-27 15:04:54 -0300

    Finalizado Primeiro dia

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Menu from './components/menu';
 import dadosIniciais from './data/dados_iniciais.json'
 import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 
 
@@ -13,14 +15,24 @@ function App() {
       <div>
         <Menu />
       </div>
-      <BannerMain>
+      <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription=
-        {
+        videoDescription={
           "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
         }
-      </BannerMain>
+      />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+      <Carousel category={dadosIniciais.categorias[1]} />
+
+      <Carousel category={dadosIniciais.categorias[2]} />
+
+      <Carousel category={dadosIniciais.categorias[3]} />
+
+      <Carousel category={dadosIniciais.categorias[4]} />
+
+      <Carousel category={dadosIniciais.categorias[5]} />
+      <Footer />
     </div>
   );
 }
