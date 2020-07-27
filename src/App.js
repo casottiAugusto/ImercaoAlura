81e65f124b03b5a8a3860c28928f354b7cbadfa9
Joao Augusto <augusto_casotti@hotmail.com>
2020-07-27 16:05:42 -0300

    Atualizando Videos

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div style={{ background: "#141414" }}>
+    <div style={{ background: "#696969" }}>
       <div>
         <Menu />
       </div>
